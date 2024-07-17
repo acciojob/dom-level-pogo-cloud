26908e3bab0587d1ff5e173253076b7cfc8b2456
Deeptesh Biswas <67312636+pogo-cloud@users.noreply.github.com>
2024-07-17 20:06:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let level = document.querySelector("#level");
+        let root = document.querySelector("html");
+        // for (let i = 0; i < root.children.length; i++) {
+        //     console.log(root.children[0].textContent);
+        // }
+        let count = 1;
+        while (level != root) {
+            count++;
+
+            level = level.parentNode;
+
+        }
+        alert(`The level of the element is: ${count}`);
